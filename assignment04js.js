8d23946595e244a367e00c4273abc72aa2301573
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2015-04-26 19:04:46 +0900

    Cannot manage to do anything with the assignment.

--- assignment04js.js
+++ assignment04js.js
@@ -1,8 +1,18 @@
 /* JS for Ass03b */
 
+var 
 
-function beginSearch() {
-	var langChoice = document.getElementsByType("langType")[0];
-	console.log(langChoice.value);
+function getGistList() {
+	var req = new XMLHttpRequest();
+	if (!req) {
+		throw 'unable to create HttpRequest.';
+	}
+	var pythonOk = document.getElementsByName('python-ok')[0];
+	var jsonOk = document.getElementsByName('json-ok')[0];
+	var javascriptOk = document.getElementsByName('javascript-ok')[0];
+	var sqlOk = document.getElementsByName('sql-ok')[0];
+	var url = 'https://developer.github.com/v3/gists/#list-gists';
+	var params = {
+		
+	}
 }
-
